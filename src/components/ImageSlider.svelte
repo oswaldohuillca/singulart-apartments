635a91ebd570a454ffe7de50b8316b69f2627f5b
Oswaldo <oswaldohuillca@apros.pe>
2024-02-18 11:49:10 -0500

    update mobile version and images

--- src/components/ImageSlider.svelte
+++ src/components/ImageSlider.svelte
@@ -3,29 +3,28 @@
   const images = [
     {
       src: "/rooms/room-1.avif",
-      title: "Sala / comedor"
+      title: "Sala / comedor",
     },
     {
       src: "/rooms/room-2.avif",
-      title: "Sala / comedor"
+      title: "Sala / comedor",
     },
     {
       src: "/rooms/room-3.avif",
-      title: "Sala / comedor"
+      title: "Sala / comedor",
     },
     {
       src: "/rooms/room-4.avif",
-      title: "Sala / comedor"
+      title: "Sala / comedor",
     },
     {
       src: "/rooms/room-5.avif",
-      title: "Sala / comedor"
-    }
-  ]
+      title: "Sala / comedor",
+    },
+  ];
 </script>
 
-<section class="py-32 relative">
-
+<section class="py-10 md:py-32 relative">
   <slot />
 
   <Splide
@@ -44,7 +43,12 @@
       // easing: "cubic-bezier(0, 0.14, 1, 0.65)",
       speed: 2500,
       autoplay: true,
-      pagination:false
+      pagination: false,
+      breakpoints: {
+        767: {
+          padding: "15%",
+        },
+      },
     }}
     on:mounted={(e) => console.log(e.detail.splide.length)}
   >
@@ -52,14 +56,7 @@
       {#each images as { src, title }, i}
         <SplideSlide>
           <figure>
-            <img
-              src={src}
-              alt={title}
-              class="w-full"
-              width="1374"
-              height="828"
-            />
-
+            <img {src} alt={title} class="w-full" width="1374" height="828" />
             <figcaption>
               <h3>{title}</h3>
             </figcaption>
@@ -70,23 +67,42 @@
 
     <div class="splide__arrows">
       <button class="splide__arrow splide__arrow--prev">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
           <g fill="none">
-            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-            <path fill="currentColor" d="m14.707 5.636l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l3.95-3.95H4a1 1 0 1 1 0-2h13.243l-3.95-3.95a1 1 0 1 1 1.414-1.414" />
+            <path
+              d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
+            />
+            <path
+              fill="currentColor"
+              d="m14.707 5.636l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l3.95-3.95H4a1 1 0 1 1 0-2h13.243l-3.95-3.95a1 1 0 1 1 1.414-1.414"
+            />
           </g>
         </svg>
       </button>
       <button class="splide__arrow splide__arrow--next">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
           <g fill="none">
-            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-            <path fill="currentColor" d="m14.707 5.636l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l3.95-3.95H4a1 1 0 1 1 0-2h13.243l-3.95-3.95a1 1 0 1 1 1.414-1.414" />
+            <path
+              d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
+            />
+            <path
+              fill="currentColor"
+              d="m14.707 5.636l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l3.95-3.95H4a1 1 0 1 1 0-2h13.243l-3.95-3.95a1 1 0 1 1 1.414-1.414"
+            />
           </g>
         </svg>
       </button>
     </div>
-    
   </Splide>
 </section>
 
@@ -102,22 +118,34 @@
       }
 
       figcaption {
-        @apply absolute top-0 left-0 w-full h-full flex items-end bg-slate-950/80 p-6;
+        @apply absolute top-0 left-0 w-full h-full flex items-end bg-slate-950/80 p-4;
         transition: background 2s ease-in-out;
+
+        @screen md {
+          @apply p-6;
+        }
         &:after {
-          content: '';
-          background: linear-gradient(0deg, rgba(0,0,0,.9) 0%, rgba(0,0,0,0.7) 15%, rgba(0,212,255,0) 100%);
+          content: "";
+          background: linear-gradient(
+            0deg,
+            rgba(0, 0, 0, 0.9) 0%,
+            rgba(0, 0, 0, 0.7) 15%,
+            rgba(0, 212, 255, 0) 100%
+          );
           transition: opacity 2s ease-in-out;
           @apply h-full w-full top-0 left-0 absolute opacity-0;
         }
-        h3{
+        h3 {
           transition: opacity 2s ease-in-out;
-          @apply font-bold text-3xl text-white relative z-10 opacity-0;
+          @apply font-bold text-lg text-white relative z-10 opacity-0;
+          @screen md {
+            @apply text-3xl;
+          }
         }
       }
     }
   }
-  
+
   :global(.splide__slide.is-active) {
     figure {
       transform: scale(1);
@@ -131,11 +159,13 @@
           @apply opacity-100;
         }
       }
-      
     }
   }
 
-  :global(.splide__arrow){
-    @apply w-auto h-auto p-8 bg-white/30 backdrop-blur-md text-3xl pointer-events-auto text-white ;
+  :global(.splide__arrow) {
+    @apply w-auto h-auto p-2 bg-white/30 backdrop-blur-md text-xl pointer-events-auto text-primary;
+    @screen md {
+      @apply p-8 text-3xl text-white;
+    }
   }
 </style>
