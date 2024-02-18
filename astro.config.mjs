6094fc04c4a2e417869881f106d67cce0b0934e0
Oswaldo <oswaldohuillca@apros.pe>
2024-02-17 19:57:14 -0500

    added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from '@astrojs/svelte';
+import robotsTxt from 'astro-robots-txt';
+
 
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://singulart.pages.dev/",
   integrations: [
     svelte(),
-    tailwind()
+    tailwind(),
+    robotsTxt()
   ],
   renderers: [
     '@astrojs/renderer-svelte',
